363dbb665370cdb89d9093630f82d0913cc7f172
biqkcg8900 <125282310+biqkcg8900@users.noreply.github.com>
2023-02-18 18:27:56 -0300

    js contact added
    pirulito q bate bate

--- rj/contact/main.js
+++ rj/contact/main.js
@@ -1,3 +1,18 @@
+function abrirWhatsapp() {
+	var nome = document.getElementById("name").value;
+	var telefone = document.getElementById("telefone").value;
+	var email = document.getElementById("email").value;
+	var msg = document.getElementById("msg").value;
+	var url = "https://wa.me/+5598981130988?text=" // Seu numero
+	  + "*Formulário de Contato*" + "%0a" // Mensagem personalizada
+	  + "%0a" // Quebra de linha
+	  + "*Nome*: " + nome + "%0a" // Dados do formulário
+	  + "*Telefone*: " + telefone + "%0a"
+	  + "*E-mail*: " + email + "%0a"
+	  + "*Mensagem*: " + msg;
+	window.open(url, '_blank').focus();
+  }
+
 const faqbtns = document.querySelectorAll('.ask')
 const faqans = document.querySelectorAll('.answer')
 const imgx = document.querySelectorAll('.ask img')
