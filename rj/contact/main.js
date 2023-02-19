c0ab4607ab9287c9a0813b51d3f5c724d7416b17
Camila Sousa <camilacdesousa2@gmail.com>
2023-02-18 22:08:52 -0300

    contact arrumado

--- rj/contact/main.js
+++ rj/contact/main.js
@@ -1,17 +1,26 @@
 function abrirWhatsapp() {
-	var nome = document.getElementById("name").value;
-	var telefone = document.getElementById("telefone").value;
-	var email = document.getElementById("email").value;
-	var msg = document.getElementById("msg").value;
-	var url = "https://wa.me/+5598981130988?text=" // Seu numero
-	  + "*Formulário de Contato*" + "%0a" // Mensagem personalizada
-	  + "%0a" // Quebra de linha
-	  + "*Nome*: " + nome + "%0a" // Dados do formulário
-	  + "*Telefone*: " + telefone + "%0a"
-	  + "*E-mail*: " + email + "%0a"
-	  + "*Mensagem*: " + msg;
-	window.open(url, '_blank').focus();
-  }
+  var nome = document.getElementById('name').value
+  var telefone = document.getElementById('telefone').value
+  var email = document.getElementById('email').value
+  var msg = document.getElementById('msg').value
+  var url =
+    'https://wa.me/+5598981130988?text=' + // Seu numero
+    '*Formulário de Contato*' +
+    '%0a' + // Mensagem personalizada
+    '%0a' + // Quebra de linha
+    '*Nome*: ' +
+    nome +
+    '%0a' + // Dados do formulário
+    '*Telefone*: ' +
+    telefone +
+    '%0a' +
+    '*E-mail*: ' +
+    email +
+    '%0a' +
+    '*Mensagem*: ' +
+    msg
+  window.open(url, '_blank').focus()
+}
 
 const faqbtns = document.querySelectorAll('.ask')
 const faqans = document.querySelectorAll('.answer')
