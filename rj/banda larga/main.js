3340cd30ecffdd020f752f21047138980eaf2fc1
Camila Sousa <camilacdesousa2@gmail.com>
2023-02-22 18:28:18 -0300

    banda larga responsivo

--- rj/banda larga/main.js
+++ rj/banda larga/main.js
@@ -1,39 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  speed: 1000,
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  loop: true,
-  autoplay: {
-    delay: 3800
-  }
-})
-
-const swipertest = new Swiper('.swiper-testmonials', {
-  speed: 1000,
-  slidesPerView: 1,
-  spaceBetween: 40,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  loop: true,
-  autoplay: {
-    delay: 3800
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    991: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    }
-  }
-})
-
 const cards = document.querySelectorAll('.card')
 const names = document.querySelectorAll('.name')
 
