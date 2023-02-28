ae87852ad79d09012e704f987deaf2635d58ef63
Camila Sousa <camilacdesousa2@gmail.com>
2023-02-28 10:27:02 -0300

    btn go to top fixed smooth

--- rj/footer/main.js
+++ rj/footer/main.js
@@ -5,14 +5,25 @@ const espacodobtn = document.querySelector('#content-back')
 backToTopButton.addEventListener('click', function () {
   window.scrollTo(0, 0)
 })
+
+const isVisible = function isInViewport(element) {
+  const rect = element.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) - 32 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
 window.addEventListener('scroll', function () {
-  console.log(espacodobtn.getBoundingClientRect().top)
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
-  if (espacodobtn.getBoundingClientRect().top <= 890) {
+  if (isVisible(espacodobtn)) {
     backToTopButton.classList.add('fixed')
   } else {
     backToTopButton.classList.remove('fixed')
